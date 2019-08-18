6e42a6c354fe9d44dced4746c530399d36e86425
casper <solitaryreach@163.com>
2019-08-18 16:13:35 +0800

    添加生成值的接口

--- src/main/java/org/r/generator/value/api/ValueGenerator.java
+++ src/main/java/org/r/generator/value/api/ValueGenerator.java
@@ -1,19 +1,52 @@
 package org.r.generator.value.api;
 
+import org.r.generator.value.beans.DataModelBO;
 import org.r.generator.value.beans.RuleBO;
 import org.r.generator.value.strategys.ValueGenerateStrategy;
 import org.r.generator.value.strategys.ValueGenerateStrategyBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ValueGenerator {
 
 
-    Object getValue(String propertyName, String type, RuleBO rule) {
+    /**
+     * 根据数据模型生成值，并填充到数据模型中
+     *
+     * @param model 数据模型
+     */
+    public Map<String,Object> getValue(DataModelBO model) {
 
+
+
+
+
+
+
+
+
+
+
+        return new HashMap<>();
+    }
+
+    /**
+     * 根据参数属性生成值
+     *
+     * @param propertyName 参数名
+     * @param type         参数类型
+     * @param rule         参数限制
+     * @return
+     */
+    public Object getValue(String propertyName, String type, RuleBO rule) {
 
         ValueGenerateStrategy strategy = ValueGenerateStrategyBuilder.getStrategy(type);
-        System.out.println("hello world");
-
-        return strategy.getValue(rule);
+        if (rule != null) {
+            return strategy.getValue(rule);
+        } else {
+            return strategy.getValue();
+        }
     }
 
 
---
+++ src/main/java/org/r/generator/value/beans/DataModelBO.java
@@ -0,0 +1,35 @@
+package org.r.generator.value.beans;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DataModelBO {
+
+    /**
+     * 属性名称
+     */
+    private String name;
+
+    /**
+     * 属性类型
+     */
+    private String type;
+
+    /**
+     * 属性在当前的数据模型的深度，从1开始
+     */
+    private Integer deep;
+
+    /**
+     * 属性限制
+     */
+    private RuleBO rule;
+
+    /**
+     * 子属性
+     */
+    private List<DataModelBO> children;
+
+}
